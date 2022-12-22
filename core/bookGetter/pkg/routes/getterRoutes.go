d3a85315e9e5252a1fbb6ab2175f811fa6c5c0ad
chaimakr <chaima050@gmail.com>
2022-12-22 22:14:49 +0100

    feat(core): add routes to bookGetter and bookSetter

--- core/bookGetter/pkg/routes/getterRoutes.go
+++ core/bookGetter/pkg/routes/getterRoutes.go
@@ -1,6 +1,12 @@
 package routes
 
-import {
-	"github.com/gorilla/mux"
+import (
 	"github.com/chaimakr/book_management_system/bookGetter/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookGetterRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+
 }
