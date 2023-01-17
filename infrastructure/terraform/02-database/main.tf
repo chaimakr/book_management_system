8ec35ebfb35bfe28d8bbe7814f9bbac9408c30f1
chaimakr <chaima050@gmail.com>
2023-01-17 17:26:19 +0100

    feat(infra): stack 3 database mongo

---
+++ infrastructure/terraform/02-database/main.tf
@@ -0,0 +1,118 @@
+data "terraform_remote_state" "aks" {
+  backend = "azurerm"
+  config = {
+    resource_group_name = "terra-book-ms"
+    container_name = "bmstfstate"
+    storage_account_name = "bmsbackend"
+    key = "dev.terraform.bmstfstate"
+  }
+}
+
+# resource "kubernetes_config_map" "database_config_map" {
+#     metadata {
+#       name = var.config_map_name
+#       labels = {
+#         app = "mongodb"
+#       }
+#     }
+#     data = {
+#     }
+    
+# }
+
+resource "kubernetes_persistent_volume" "mongo-pv" {
+    metadata {
+      name = "mongo-pv"
+      labels = {
+        type = "local"
+        app = "mongodb-pv"
+      }
+    }
+
+    spec {
+      storage_class_name = "manual"
+      capacity = {
+        storage = "1Gi"
+      }
+      access_modes = [ "ReadWriteOnce" ]
+      persistent_volume_source {
+        host_path {
+          path = "/mnt/data"
+        }
+      }
+    }
+}
+
+resource "kubernetes_persistent_volume_claim_v1" "mongo-pvc" {
+  metadata {
+    name = "mongo-pvc"
+    labels = {
+      app = "mongodb"
+    }
+  }
+  spec {
+    storage_class_name = "manual"
+    access_modes = [ "ReadWriteOnce" ]
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
+  }
+}
+
+resource "kubernetes_deployment" "db_deploy" {
+  metadata {
+    name = "mongodb"
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        "app" = "database"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "database"
+        }
+      }
+      spec {
+        container {
+          name = "mongodb"
+          image = "mongo:4.4"
+          image_pull_policy = "IfNotPresent"
+          volume_mount {
+            name = "mongodb-data"
+            mount_path = "/data/db"
+          }
+        }
+        volume {
+          name = "mongodb-data"
+          persistent_volume_claim {
+            claim_name = "mongo-pvc"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "db_service" {
+  metadata {
+    name = "mongodb"
+    labels = {
+      app = "database"
+    }
+  }
+  spec {
+    type = "NodePort"
+    port {
+      port = 27017
+    }
+    selector = {
+      app = 30005
+    }
+  }
+}
---
+++ infrastructure/terraform/02-database/provider.tf
@@ -0,0 +1,39 @@
+
+terraform {
+  required_providers {
+    kubernetes = {
+        source = "hashicorp/kubernetes"
+        version = "~>2.16.1"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name = "terra-book-ms"
+    storage_account_name = "bmsbackend"
+    container_name = "bmstfstate"
+    key = "dev.terraform-db-init.tfstate"
+  }
+}
+
+locals {
+  kube_config = one(data.terraform_remote_state.aks.outputs.kube_config)
+  host = local.kube_config.host
+  username = local.kube_config.username
+  password = local.kube_config.password
+  client_certificate = base64decode(local.kube_config.client_certificate)
+  client_key = base64decode(local.kube_config.client_key)
+  cluster_ca_certificate = base64decode(local.kube_config.cluster_ca_certificate)
+}
+
+provider "kubernetes" {
+  host = local.host
+  username = local.username
+  password = local.password
+  client_certificate = local.client_certificate
+  client_key = local.client_key
+  cluster_ca_certificate = local.cluster_ca_certificate
+}
+
+provider "azurerm" {
+   features {}
+}
