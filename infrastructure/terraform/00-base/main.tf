127b8075395a928d723e993a5158b447160fd4b0
chaimakr <chaima050@gmail.com>
2023-01-17 17:25:07 +0100

    feat(infra): stack 1 added : ressource grp, storage account & container

---
+++ infrastructure/terraform/00-base/main.tf
@@ -0,0 +1,21 @@
+resource "azurerm_resource_group" "terra-book-ms" {
+  name     = "terra-book-ms"
+  location = "West US 3"
+}
+resource "azurerm_storage_account" "bms-storage-account" {
+  name                     = "bmsbackend"
+  resource_group_name      = azurerm_resource_group.terra-book-ms.name
+  location                 = azurerm_resource_group.terra-book-ms.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "bms-storage-container" {
+  name                  = "bmstfstate"
+  storage_account_name  = azurerm_storage_account.bms-storage-account.name
+  container_access_type = "private"
+}
---
+++ infrastructure/terraform/00-base/provider.tf
@@ -0,0 +1,12 @@
+provider "azurerm" {
+  features {}
+}
+terraform {
+  required_version = "~>1.3.4"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.31.0"
+    }
+  }
+}
